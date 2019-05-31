8a9488841a3c68ff887414d87e7fd05dfadd7a7a
Joseph Wright <joseph.wright@morningstar2.co.uk>
2019-05-31 13:13:34 +0100

    Update script for l3build

--- build.lua
+++ build.lua
@@ -20,5 +20,7 @@ typesetfiles = {"*.tex"}
 textfiles = {"*.md"}
 
 -- Find and run the build system
-kpse.set_program_name("kpsewhich")
-dofile(kpse.lookup("l3build.lua"))
+kpse.set_program_name ("kpsewhich")
+if not release_date then
+  dofile(kpse.lookup("l3build.lua"))
+end
